adf594374256f92ad503b8296826e9d914eaacdc
gc <evemen@qq.com>
2019-03-04 21:58:39 +0800

    2

---
+++ Strategy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strategy
+{
+    /// <summary>
+    /// 场景：商场根据不同商品采购不用的折扣方式计费
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
